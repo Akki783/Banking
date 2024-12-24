08a52bb9157512466f30f44e4e8bb54fd68070e5
Akki783 <platformsupport@dove-soft.com>
2024-12-24 11:53:08 +0530

    update : testing

--- controller/BankController.js
+++ controller/BankController.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET, TOKEN_EXPIRY } = require("./login");
 const baseUrl = process.env.BASE_URL || "http://localhost:4000";
 const connectToRedis = require("../config/redis");
+const crypto = require('crypto'); // For generating UUID
+const CryptoJS = require('crypto-js'); // For SHA256 hashing
 
 const config = {
   cloudinary: {
